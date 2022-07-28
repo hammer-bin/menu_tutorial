c946636ae183489d99cfb65daf3a9589ede5c9ce
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-07-29 06:18:26 +0900

    Initial Commit

---
+++ menu_tutorial/menu_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  menu_tutorialApp.swift
+//  menu_tutorial
+//
+//  Created by minkyuLee on 2022/07/29.
+//
+
+import SwiftUI
+
+@main
+struct menu_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
